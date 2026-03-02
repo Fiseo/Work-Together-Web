ed899c9bdb6e0b6f599759b4991a04f9afdab054
Vigile Martinier <virgile.martinier6002@gmail.com>
2026-03-02 16:46:34 +0100

    Création des roots dans booking controller
    Took 36 minutes

---
+++ src/Controller/BookingController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/booking')]
+final class BookingController extends AbstractController
+{
+    #[Route('/new', name: 'app_booking_new')]
+    public function new(): Response
+    {
+        //TODO : création d'un booking
+        return $this->render('base.html.twig', []);//TODO : Twig
+    }
+
+    #[Route('/{booking}/pay', name: 'app_booking_pay')]
+    public function edit():Response {
+
+        //TODO : Paiement d'un booking
+        return $this->render('base.html.twig', []);//TODO : Twig
+    }
+
+    #[Route('/{booking}/', name: 'app_booking_details')]
+    public function details():Response {
+
+        //TODO : Consulter les informations d'un booking
+        return $this->render('base.html.twig', []);//TODO : Twig
+    }
+}
