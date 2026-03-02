540faf13cf505bab2fbdd16f22f2eaad962ec01b
Vigile Martinier <virgile.martinier6002@gmail.com>
2026-03-02 17:20:14 +0100

    pb fix
    Took 5 minutes

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/user', name: 'app_user')]
+#[Route('/user')]
 final class UserController extends AbstractController
 {
     #[Route('/', name: 'app_user')]
