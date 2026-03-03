047358a7379450ea37a18a786a0bc87ec92bfe62
Vigile Martinier <virgile.martinier6002@gmail.com>
2026-03-03 13:48:21 +0100

    Optimisation de findAvailable
    Took 11 minutes

--- src/Repository/UnitRepository.php
+++ src/Repository/UnitRepository.php
@@ -18,19 +18,30 @@ class UnitRepository extends ServiceEntityRepository
         parent::__construct($registry, Unit::class);
     }
 
+    public function findAllWithBookingUnits(): Collection
+    {
+        $result = $this->createQueryBuilder('u')
+            ->leftJoin('u.bookingUnits', 'b')
+            ->addSelect('b')
+            ->getQuery()
+            ->getResult();
+        return new ArrayCollection($result);
+    }
     /**
      * @return Collection<int, Unit>
      */
-        public function findAvailable(): Collection
-        {
-            $nonFiltered = $this->findAll();
+    public function findAvailable(): Collection
+    {
+        $nonFiltered = $this->findAllWithBookingUnits();
 
-            $result = [];
-            foreach ($nonFiltered as $unit) {
-                if ($unit->isAvailable())
-                    $result[] = $unit;
-            }
-
-            return new ArrayCollection($result);
+        $result = [];
+        foreach ($nonFiltered as $unit) {
+            if ($unit->isAvailable())
+                $result[] = $unit;
         }
+
+        return new ArrayCollection($result);
+    }
+
+
 }
