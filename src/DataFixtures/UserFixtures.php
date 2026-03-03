b3dc44af6c922a4f1b4306c8bee0879a890e3738
Vigile Martinier <virgile.martinier6002@gmail.com>
2026-03-03 10:18:21 +0100

    Création Fixtures de User
    Took 9 minutes

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Civility;
+use App\Entity\Company;
+use App\Entity\Individual;
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $u = (new User())
+            ->setUsername('admin')
+            ->setEmail('admin@gmail.com')
+            ->setPassword('$2y$13$KxH9ONFNvdook3MyNVqOJ.S6NML.r5nBvdFM6W0xkexhbGwCJvraO');//613670
+        $manager->persist($u);
+
+        $i = (new Individual())
+            ->setUsername('Jane')
+            ->setEmail('jane@gmail.com')
+            ->setFirstName('Jane')
+            ->setLastName('Doe')
+            ->setBirthDate(new \DateTime('1990-01-01'))
+            ->setCivility($this->getReference('f', Civility::class))
+            ->setPassword('$2y$13$KxH9ONFNvdook3MyNVqOJ.S6NML.r5nBvdFM6W0xkexhbGwCJvraO');//613670
+        $this->addReference('jane', $i);
+        $manager->persist($i);
+
+        $c = (new Company())
+            ->setUsername('Google')
+            ->setEmail('google@gmail.com')
+            ->setName('Google')
+            ->setCreation(new \DateTime('1998-09-04'))
+            ->setCompanyRegister('44306184100047')
+            ->setPassword('$2y$13$KxH9ONFNvdook3MyNVqOJ.S6NML.r5nBvdFM6W0xkexhbGwCJvraO');//613670
+        $this->addReference('google', $c);
+        $manager->persist($c);
+
+        $manager->flush();
+    }
+}
