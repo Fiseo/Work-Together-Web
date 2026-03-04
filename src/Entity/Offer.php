c0774c3996832d082a3e494a5703d60374bc2dda
Vigile Martinier <virgile.martinier6002@gmail.com>
2026-03-04 15:51:52 +0100

    mag de offer

--- src/Entity/Offer.php
+++ src/Entity/Offer.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\OfferRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 
@@ -44,6 +45,9 @@ class Offer
      */
     #[ORM\OneToMany(targetEntity: Booking::class, mappedBy: 'offer')]
     private Collection $bookings;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
 
     public function __construct()
     {
@@ -117,6 +121,18 @@ class Offer
                 $booking->setOffer(null);
             }
         }
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): static
+    {
+        $this->description = $description;
 
         return $this;
     }
