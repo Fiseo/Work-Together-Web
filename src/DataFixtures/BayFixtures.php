435593364cd475a4889e7b8e5f445b5e4d0bf7ba
Vigile Martinier <virgile.martinier6002@gmail.com>
2026-04-16 11:50:13 +0200

    Ajout d'un champ unitPrefix dans la table Bay et prise en compte dans le reste de l'app

--- src/DataFixtures/BayFixtures.php
+++ src/DataFixtures/BayFixtures.php
@@ -15,72 +15,77 @@ class BayFixtures extends Fixture
             ->setHaveProblem(false);
 
         $b = (new Bay())
-            ->setLabel('B01');
+            ->setLabel('B01')
+            ->setUnitPrefix("U");
         $manager->persist($b);
 
         for ($i = 1; $i <= 42; $i++) {
             $u = clone $uTemplate;
             $u->setBay($b);
             if ($i >= 10)
-                $u->setLabel('U'.$i);
+                $u->setLabel($b->getUnitPrefix().$i);
             else
-                $u->setLabel('U0'.$i);
+                $u->setLabel($b->getUnitPrefix().'0'.$i);
             $manager->persist($u);
         }
 
         $b = (new Bay())
-            ->setLabel('B02');
+            ->setLabel('B02')
+            ->setUnitPrefix("U");
         $manager->persist($b);
 
         for ($i = 1; $i <= 42; $i++) {
             $u = clone $uTemplate;
             $u->setBay($b);
             if ($i >= 10)
-                $u->setLabel('U'.$i);
+                $u->setLabel($b->getUnitPrefix().$i);
             else
-                $u->setLabel('U0'.$i);
+                $u->setLabel($b->getUnitPrefix().'0'.$i);
             $manager->persist($u);
         }
 
         $b = (new Bay())
-            ->setLabel('B03');
+            ->setLabel('B03')
+            ->setUnitPrefix("U");
         $manager->persist($b);
 
         for ($i = 1; $i <= 42; $i++) {
             $u = clone $uTemplate;
             $u->setBay($b);
             if ($i >= 10)
-                $u->setLabel('U'.$i);
+                $u->setLabel($b->getUnitPrefix().$i);
             else
-                $u->setLabel('U0'.$i);
+                $u->setLabel($b->getUnitPrefix().'0'.$i);
             $manager->persist($u);
         }
 
         $b = (new Bay())
-            ->setLabel('B04');
+            ->setLabel('B04')
+            ->setUnitPrefix("U");
         $manager->persist($b);
 
         for ($i = 1; $i <= 42; $i++) {
             $u = clone $uTemplate;
             $u->setBay($b);
             if ($i >= 10)
-                $u->setLabel('U'.$i);
+                $u->setLabel($b->getUnitPrefix().$i);
             else
-                $u->setLabel('U0'.$i);
+                $u->setLabel($b->getUnitPrefix().'0'.$i);
             $manager->persist($u);
         }
 
         $b = (new Bay())
-            ->setLabel('B05');
+            ->setLabel('B05')
+            ->setUnitPrefix("U");
         $manager->persist($b);
 
         for ($i = 1; $i <= 42; $i++) {
             $u = clone $uTemplate;
             $u->setBay($b);
             if ($i >= 10)
-                $u->setLabel('U'.$i);
+                $u->setLabel($b->getUnitPrefix().$i);
             else
-                $u->setLabel('U0'.$i);
+                $u->setLabel($b->getUnitPrefix().'0'.$i);
             $manager->persist($u);
         }
 
